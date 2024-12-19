bc65246a230edac3adc3bc485363216ceabb1856
Victor Hugo <rayosanchez1@gmail.com>
2024-12-18 18:51:56 -0600

    nuevo formato

---
+++ primer.js
@@ -0,0 +1,19 @@
+//declaración//
+let cliente_potencial;
+
+//asignación de valores//
+cliente_potencial = "Giancarlo"
+cliente_potencial = "Pedro"
+console.log(cliente_potencial);
+
+//Inicializador de variables:
+let edad = 34;
+
+console.log(edad);
+
+
+//constante: es una variable cuyo valor no puede ser modificado unaz que ha sido asigando
+
+const nacimiento  = 1999
+console.log(nacimiento);
+
